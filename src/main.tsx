b68c38c8d75f0d0886bc7d2aa70864aecf8425ae
Mustafa Taşkın <mustafa.tskn818@gmail.com>
2026-07-12 15:57:04 +0300

    Tema sistemi eklendi

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(() => undefined);
-  });
-}
+import "./styles/theme.css";
+import "./index.css";
+
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
